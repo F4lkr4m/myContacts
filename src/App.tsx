1ea8449541670ec0e22d4c6f77b8fee89475b34f
Sergey Scherbakov <falkr4m@yandex.ru>
2022-01-25 13:26:52 +0300

    Unused imports deletd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.css';
-import Button from './Components/Button/Button';
-import Fonts from './Components/Fonts/Fonts';
-import Input from './Components/Input/Input';
-import TextArea from './Components/TextArea/TextArea';
 import HomeView from './Views/HomeView/HomeView';
-import SignView from './Views/SignView/SignView';
 
 function App() {
   return (
